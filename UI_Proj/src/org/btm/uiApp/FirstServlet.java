76cb1627cf9128990b12169bbe0d6c14368c08a5
XYZ <XYZ@DESKTOP-U8F78NA>
2020-02-05 13:27:43 +0530

    UI_form_data

---
+++ UI_Proj/src/org/btm/uiApp/FirstServlet.java
@@ -0,0 +1,27 @@
+package org.btm.uiApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServlet extends GenericServlet
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException
+	{
+		String name=req.getParameter("nm");
+		String place=req.getParameter("pl");
+		PrintWriter out=resp.getWriter();
+		out.println("<html><body bgcolor='red'>"
+				+ " <h1> DABBA FELLO "+  name +" "+ place+"</h1>"+"</body></html>");
+		out.flush();
+		out.close();
+	}
+	
+
+}
